baf631df7ad509b214a39854e77ca11de3a87e21
Sanjay G <sanjay27it@gmail.com>
2025-07-20 11:22:20 +0530

    integrating the components

---
+++ url-shortener/src/App.jsx
@@ -0,0 +1,12 @@
+import UrlShortener from './UrlShortener';
+function App(){
+  return (
+    <main>
+      <div className="container">
+        <UrlShortener />
+      </div>
+    </main>
+  )
+}
+
+export default App;
