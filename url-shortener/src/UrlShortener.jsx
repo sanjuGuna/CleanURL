d7f3ec9bbf6c9771be24a2001e51818dbd5dbf1c
Sanjay G <sanjay27it@gmail.com>
2025-08-31 11:26:32 +0530

    added styles

--- url-shortener/src/UrlShortener.jsx
+++ url-shortener/src/UrlShortener.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios';
-
+import './UrlShortener.css'
 const UrlShortenerForm = () => {
     const [longUrl, setLongUrl] = useState('');
     const [shortUrl, setShortUrl] = useState('');
